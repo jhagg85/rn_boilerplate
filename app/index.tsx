e0018aab07c05912ee659eaef4df0109d669e31e
jhagg85 <jhaggy85@gmail.com>
2024-12-14 02:28:26 +0800

    setting up splash screen and cleanup

---
+++ app/index.tsx
@@ -0,0 +1,14 @@
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+import "../global.css";
+
+const Home = () => {
+  return (
+    <SafeAreaView>
+      <Text>Home</Text>
+    </SafeAreaView>
+  );
+};
+
+export default Home;
